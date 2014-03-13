92436d4b92dcbadcbe8247a442e707229056b7e5
Krajcovic <dusan.krajcovic@gmail.com>
2014-03-13 15:13:16 +0100

    updated DB dump from 13.3.2014 about picture_id

--- db/dump.sql
+++ db/dump.sql
@@ -1,116 +1,88 @@
--- MySQL dump 10.13  Distrib 5.1.57, for redhat-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 2.11.4
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: a6340459_poker
--- ------------------------------------------------------
--- Server version	5.1.57
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Host: localhost
+-- Generation Time: Mar 13, 2014 at 10:12 AM
+-- Server version: 5.1.57
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `a6340459_poker`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `cards`
 --
 
-DROP TABLE IF EXISTS `cards`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cards` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `color_id` int(11) NOT NULL,
-  `value_id` int(11) NOT NULL,
-  `picture_id` int(11) NOT NULL,
+  `color_id` int(11) unsigned NOT NULL,
+  `value_id` int(11) unsigned NOT NULL,
+  `picture_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `cards`
 --
 
-LOCK TABLES `cards` WRITE;
-/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
-/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `colors`
 --
 
-DROP TABLE IF EXISTS `colors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `colors` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `mark` char(1) COLLATE utf8_czech_ci NOT NULL,
   `name` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `picture_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='card colors';
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci COMMENT='card colors' AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `colors`
 --
 
-LOCK TABLES `colors` WRITE;
-/*!40000 ALTER TABLE `colors` DISABLE KEYS */;
-/*!40000 ALTER TABLE `colors` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `pictures`
 --
 
-DROP TABLE IF EXISTS `pictures`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(256) COLLATE utf8_czech_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `pictures`
 --
 
-LOCK TABLES `pictures` WRITE;
-/*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `values`
 --
 
-DROP TABLE IF EXISTS `values`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `values` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `mark` char(1) COLLATE utf8_czech_ci NOT NULL,
   `name` varchar(40) COLLATE utf8_czech_ci NOT NULL,
+  `picture_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `values`
 --
-
-LOCK TABLES `values` WRITE;
-/*!40000 ALTER TABLE `values` DISABLE KEYS */;
-/*!40000 ALTER TABLE `values` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-03-13  9:30:37
 
