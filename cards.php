72751feb90aa65804f6c1f708eee278cd7a97cac
Krajcovic <dusan.krajcovic@gmail.com>
2014-03-14 09:03:36 +0100

    Added bootstrap to project.

--- cards.php
+++ cards.php
@@ -8,8 +8,40 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
+        <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap theme -->
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/poker.css" rel="stylesheet">
     </head>
     <body>
+          <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="./pictures.php">Pictures</a></li>
+                        <li><a href="./colors.php">Colors</a></li>
+                        <li><a href="./values.php">Values</a></li>
+                        <li><a href="./cards.php">Cards</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
         <?php
         // put your code here
         ?>
